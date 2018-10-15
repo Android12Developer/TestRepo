fa9b0d65f8939270fa861aa38af38481587674ca
Zar Zar Hlaing <zarzarhlaing12@gmail.com>
2018-10-15 11:47:57 -0700

    merge

--- app/src/main/java/com/example/zarzar/jsonparser/MainActivity.java
+++ app/src/main/java/com/example/zarzar/jsonparser/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.zarzar.jsonparser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("___tag__", "___");
 
     }
 }
