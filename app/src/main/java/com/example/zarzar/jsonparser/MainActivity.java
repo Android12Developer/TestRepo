51e0e7ac09453c392a53c0a1c86ccf4f6ee9e104
Zar Zar Hlaing <zarzarhlaing12@gmail.com>
2018-10-15 11:57:04 -0700

    merge

--- app/src/main/java/com/example/zarzar/jsonparser/MainActivity.java
+++ app/src/main/java/com/example/zarzar/jsonparser/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("___tag__", "___");
+        Log.i("___test git __", "___");
 
     }
 }
